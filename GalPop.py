c7da66542a3da7742091551f8a67686ca145ae3d
dmsikors <dmsikors@iu.edu>
2024-07-18 12:17:40 -1000

    prepping for LePhare runs

--- GalPop.py
+++ GalPop.py
@@ -173,7 +173,7 @@ class GalPop:
                 print(f"self.pks has no key {sig_range[1]}. Run self.assignPeaks with this key")
                 return
         
-        if len(self.n_sigmas) == 0:   # Update n-sigmas if needed
+        if (len(self.n_sigmas) == 0) or (np.all(np.isnan(self.n_sigmas))):   # Update n-sigmas if needed
             self.update_n_sigmas(sig_cube)
 
         if self.verbose:
@@ -228,6 +228,7 @@ class GalPop:
         ## NO UPPER BOUND (half-open sigma-interval)
         else:   
             mask = np.in1d(self.pks[sig_range[0]], g_pks[:,0])   # Make mask for if peak number is in relevant peak
+
             test_idxs = np.where((self.coords[:,2] >= z_range[0]) & (self.coords[:,2] <= z_range[1])    # In relevant redshift
                             & (self.n_sigmas >= sig_range[0])   # In relevant overdensity
                             & mask )    # In relevant 
@@ -266,14 +267,14 @@ class GalPop:
             bad_gals = np.delete(self.coords, test_idxs, axis=0)   # Galaxies which aren't used.
             bad_gals = bad_gals[np.where((bad_gals[:,2] >= z_range[0]) & (bad_gals[:,2] <= z_range[1]))]    # In relevant redshift
 
-            self.subPop_plot(sig_range, z_range, g_coords, g_pks, good_gals, bad_gals, plot)
+            self.subPop_plot(key_name, sig_range, z_range, g_coords, g_pks, good_gals, bad_gals, plot)
 
 
     # ====================================================================
 
 
 
-    def subPop_plot(self, sig_range, z_range, g_coords, g_pks, good_gals, bad_gals, plot):
+    def subPop_plot(self, key_name, sig_range, z_range, g_coords, g_pks, good_gals, bad_gals, plot):
         """
         Helper method for plotting data from the subPop
         """
@@ -351,12 +352,21 @@ class GalPop:
         if plot in ("show", "Show"): plt.show()
         else:
             if sig_range[1] >0:
-                try: plt.savefig(plot + f"\Sigma_{sig_range[0]}_{sig_range[1]}_plot.png")
-                except:
-                    try:
-                        os.mkdir(plot)
-                        plt.savefig(plot + f"\Sigma_{sig_range[0]}_{sig_range[1]}_plot.png")
-                    except: print("Unable to make plot")
+                if sig_range[0]==-99:
+                    try: plt.savefig(plot + f"\Sigma_{sig_range[0]}_{sig_range[1]}_plot{key_name}.png")
+                    except:
+                        try:
+                            os.mkdir(plot)
+                            plt.savefig(plot + f"\Sigma_{sig_range[0]}_{sig_range[1]}_plot{key_name}.png")
+                        except: print("Unable to make plot")
+
+                else:
+                    try: plt.savefig(plot + f"\Sigma_{sig_range[0]}_{sig_range[1]}_plot.png")
+                    except:
+                        try:
+                            os.mkdir(plot)
+                            plt.savefig(plot + f"\Sigma_{sig_range[0]}_{sig_range[1]}_plot.png")
+                        except: print("Unable to make plot")
             else:
                 try: plt.savefig(plot + f"\Sigma_{sig_range[0]}_plot.png")
                 except:
@@ -381,6 +391,17 @@ class GalPop:
         """
         for sk, k in zip(subPop_keys, smf_keys):
 
+            if isinstance(sk, list):
+                masses = self.ms[self.subpops[sk[0]]] 
+                vol = self.vols[sk[0]]
+                for sk_sub in sk[1:]: 
+                    masses = np.concatenate((masses, self.ms[self.subpops[sk_sub]] ))
+                    vol += self.vols[sk_sub]
+
+            else:
+                masses = self.ms[self.subpops[sk]] 
+                vol = self.vols[sk]
+
             smf_info = []       # Store smf info for this key as [[m, N, n, n_err], ...]
             mass = m_range[0]   # keep track of mass
 
@@ -388,13 +409,13 @@ class GalPop:
                 smf_mbin = []   # [m, N, n, n_error] for this mass bin
 
                 # Find galaxies in the mass bin
-                gals = np.where( (self.ms[self.subpops[k]] >= mass) & (self.ms[self.subpops[k]] < mass + m_range[2]) )[0]
+                gals = np.where( (masses >= mass) & (masses < mass + m_range[2]) )[0]
 
                 if len(gals) != 0:  # If there are galaxies in the mass bin
-                    smf_mbin.append(np.median(self.ms[self.subpops[k]][gals]))  # mass
+                    smf_mbin.append(np.median(masses[gals]))  # mass
                     smf_mbin.append(len(gals))                  # N
-                    smf_mbin.append(len(gals) / self.vols[k] / m_range[2])  # n
-                    smf_mbin.append(np.sqrt(len(gals)) / self.vols[k] / m_range[2])  # n_error
+                    smf_mbin.append(len(gals) / vol / m_range[2])  # n
+                    smf_mbin.append(np.sqrt(len(gals)) / vol / m_range[2])  # n_error
                 else:   # No galaxies in the mass bin
                     smf_mbin.append(mass)       
                     smf_mbin.append(np.nan)
@@ -404,7 +425,7 @@ class GalPop:
                 mass += m_range[2]  # Step up to new mass bin
                 smf_info.append(smf_mbin)
 
-            self.smfs[sk] = np.array(smf_info)     # Add the info for the current key
+            self.smfs[k] = np.array(smf_info)     # Add the info for the current key
 
 
     # ====================================================================
@@ -422,34 +443,37 @@ class GalPop:
             - title (str)   = Title of the plot. If '', title it "SMF"
             - plot (str)    = Path to save the string at. If '', then it shows the plot.
         """
-        colors = ['006BA4', 'FF800E', 'ABABAB', '595959', '5F9ED1', 'C85200']  
-        shapes = ["o", "H", "P", "s", "D", (5,1,0)]
+        colors = ['tab:purple', 'tab:blue','tab:green', 'tab:orange', 'tab:red']  
+        shapes = ["o", "H", "s", "D", (5,1,0)]
         min_m, max_m = 99, -99  # For plot limits
-        for i, k in enumerate(smf_keys):
-            try:
-                plt.errorbar(self.smfs[k][:,0], self.smfs[k][:,2], self.smfs[k][:,3], 
-                             label=smf_labels[i], marker=shapes[i], color=colors[i], ls='')
-                min_m = min(min_m, min(self.smfs[k][:,0]))
-                max_m = max(max_m, max(self.smfs[k][:,0]))
-            except: print(f"The key {k} is not in self.smfs. Generate the smf and run again")
-        # Fix up mass limits
-        min_m = 8.5 if min_m==99 else min_m-0.5
-        max_m = 12 if max_m==-99 else max_m+0.5
+        if len(smf_keys) > 0:
+            for i, k in enumerate(smf_keys):
+                try:
+                    plt.errorbar(self.smfs[k][:,0], self.smfs[k][:,2], self.smfs[k][:,3], 
+                                label=smf_labels[i], marker=shapes[i], color=colors[i], ls='')
+                    min_m = np.min(min_m, min(self.smfs[k][:,0]))
+                    max_m = np.max(max_m, max(self.smfs[k][:,0]))
+                except: pass
+                # except: print(f"The key {k} is not in self.smfs. Generate the smf and run again")
+            # Fix up mass limits
+            min_m = 8.5 if min_m==99 else min_m-0.5
+            max_m = 12 if max_m==-99 else max_m+0.5
+        if len(fit_keys) > 0:
+            for i, k in enumerate(fit_keys):
+                try:    
+                    params = self.fits[k]
+                except: print(f"The key {k} is not in self.fits. Generate the fit and run again")
+                m_vals = np.linspace(min_m, max_m, 1000)
 
-        for i, k in enumerate(fit_keys):
-            try:    
-                params = self.fits[fit_keys]
-            except: print(f"The key {k} is not in self.fits. Generate the fit and run again")
-            m_vals = np.linspace(min_m, max_m, 1000)
-
-            if len(params) == 3:    # Single Schechter fit
-                plt.plot(m_vals, self.schechter(m_vals, *params), color=colors[i], marker='', label=fit_labels[i])
-            else:    # Double Schechter fit
-                plt.plot(m_vals, self.Dschechter(m_vals, *params), color=colors[i], marker='', label=fit_labels[i])
-      
+                if len(params) == 3:    # Single Schechter fit
+                    plt.plot(m_vals, self.schechter(m_vals, params[0], params[1], params[2]), color=colors[i], marker='', label=fit_labels[i])
+                else:    # Double Schechter fit
+                    plt.plot(m_vals, self.Dschechter(m_vals, *params), color=colors[i], marker='', label=fit_labels[i])
+        
         
         plt.yscale("log")
-        if (len(smf_keys)==0) or (len(fit_keys==0)): plt.legend(loc='lower left')
+        plt.ylim(10**-6, 10**-1)
+        if (len(smf_keys)==0) or (len(fit_keys)==0): plt.legend(loc='lower left')
         else: plt.legend(loc='lower left', ncol=2)
         if title=="": plt.title("SMF") 
         else: plt.title(title)
@@ -505,6 +529,8 @@ class GalPop:
             except: print(f"The key {k} is not in self.smfs. Generate the smf and run again")
 
         plt.yscale("log")
+        plt.ylim(10**-6, 10**-1)
+
         plt.legend(loc='lower left')
         if title=="": plt.title("SMF") 
         else: plt.title(title)
@@ -538,16 +564,18 @@ class GalPop:
             - **kwargs         = Optional variables for curve_fit
         """
         for s_key, f_key, Ns in zip(smf_keys, fit_keys, N_schechters):
-
-
-            if Ns == 1:     fit_fn = self.schechter
-            elif Ns == 2:   fit_fn = self.Dschechter
+            
+            # Find valid data points in the SMFs
+            g_idxs = np.where(np.isnan(self.smfs[s_key][:,2]) == False)
+            smf_data =  self.smfs[s_key][g_idxs]
+            
+            if Ns == 1: fit_fn = self.schechter
+            elif Ns == 2:  fit_fn = self.Dschechter
             else:   
                 print("Elements of N_schechter must be either 1 or 2")
                 return
 
-            f_err, f_params = curve_fit(fit_fn, self.smfs[s_key][:,0], self.smfs[s_key][:,2], sigma=self.smfs[s_key][:,3],
-                                        *kwargs.values())
+            f_params, f_err = curve_fit(fit_fn, smf_data[:,0], smf_data[:,2], sigma=smf_data[:,3], **kwargs)
             self.fits[f_key] = f_params
 
 
@@ -555,13 +583,13 @@ class GalPop:
 
 
     
-    def schechter(M, M_star, phi_star, alpha):
+    def schechter(self, M, M_star, phi_star, alpha):
         """
         Single-Schecter Function
         """
         return np.log(10)*phi_star*10**((M-M_star)*(1+alpha))*np.exp(-10**(M-M_star))
     
-    def Dschechter(M, M_star, phi_s1, phi_s2, alpha_1, alpha_2):
+    def Dschechter(self, M, M_star, phi_s1, phi_s2, alpha_1, alpha_2):
         """
         Double-Schechter function
         """
@@ -604,7 +632,7 @@ class GalPop:
             - sig_cube (.fits)  - The output of 'make_cube_final_overdense.py'.
         """
         ## Update voxels if there are none
-        if len(self.voxels) == 0:
+        if (len(self.voxels) == 0) or (np.all(np.isnan(self.voxels))):
             self.update_Voxels(sig_cube)
         
         if self.verbose: print("Updating overdensity values (self.ODs)")
@@ -624,11 +652,11 @@ class GalPop:
             - sig_cube (.fits)  - The output of 'make_cube_final_overdense.py'.
         """
         ## Update voxels if there are none
-        if len(self.voxels) == 0:
+        if (len(self.voxels) == 0) or (np.all(np.isnan(self.voxels))):
             self.update_Voxels(sig_cube)
 
         ## Update overdensities if needed
-        if len(self.ODs) == 0:
+        if (len(self.ODs) == 0) or (np.all(np.isnan(self.ODs))):
             self.update_ODs(sig_cube)
         
         if self.verbose: print("Updating n_sigmas values (self.n_sigmas)")
@@ -636,6 +664,30 @@ class GalPop:
         ## Calculate Overdensities and assign to attribute
         self.n_sigmas  = (self.ODs - sig_cube[11].data[self.voxels[:,2]]) / sig_cube[12].data[self.voxels[:,2]]    # (od - mean) / sigma
         
+
+    # ====================================================================
+
+    def del_objs(self, IDs):
+        """
+        Given some IDs in self.IDs, this deletes the corresponding element of all applicable attributes 
+        """
+        idxs = np.where(np.in1d(self.IDs,IDs))     # Which indices to delete.
+
+        ## Loop through the attributes and combine where possible
+        for attr in self.__dict__:
+
+            self_attr = getattr(self, attr)     # Attrs of this obj
+
+            if isinstance(self_attr, np.ndarray) and (len(self_attr)!=0): # attr is a nonempty list
+                setattr(self, attr, np.delete(self_attr,idxs, axis=0) )
+
+            elif isinstance(self_attr, dict):   # attr is a dict
+                temp_dict = {}
+                for k in self_attr:
+                    if isinstance(self_attr[k], np.ndarray) and (len(self_attr[k])!=0):
+                        temp_dict[k] = np.delete(self_attr[k], idxs)
+                setattr(self, attr, temp_dict)
+
 
     # ====================================================================
 
@@ -754,24 +806,22 @@ class GalPop:
         self.coords = np.c_[data["ra"], data["dec"], data["z"]]    # Galaxy coordinates
         self.ms = data["ms"]     # Galaxy masses
 
-        # Check voxels
-        vx = np.where(data["vx"] != data["vx"])[0]    # Check for how many NaNs
-        vy = np.where(data["vy"] != data["vy"])[0] 
-        vz = np.where(data["vz"] != data["vz"])[0] 
-        if (len(vx) != len(self.ms)) | (len(vy) != len(self.ms)) | (len(vz) != len(self.ms)):   # If voxels were reported
-            self.voxels = np.c_[data["vx"], data["vy"], data["vz"]]
+        # Voxels
+        self.voxels = np.c_[data["vx"], data["vy"], data["vz"]].astype(int)
 
-        # Check Overdensities and n_sigmas
-        if len(np.where(data["ODs"] != data["ODs"])[0]) != len(self.ms):
-            self.ODs = data["ODs"]
-        if len(np.where(data["n_sigmas"] != data["n_sigmas"])[0]) != len(self.ms):
-            self.n_sigmas = data["n_sigmas"]
+        # Overdensities and sigmas
+        self.ODs = data["ODs"]
+        self.n_sigmas = data["n_sigmas"]
+        self.pks = {}
 
         # Load peak numbers if reported
         if len(data.dtype.names) > 10:
             for idx in range(len(data.dtype.names) - 10):
-                self.pks[data.dtype.names[idx+10]] = data[data.dtype.names[idx+10]]
+                # print(idx, data.dtype.names[idx+10])
+                # print(len(data[data.dtype.names[idx+10]]))
 
+                self.pks[float(data.dtype.names[idx+10])] = data[data.dtype.names[idx+10]]
+                # print(self.pks)
 
     # ====================================================================
     # ====================================================================
